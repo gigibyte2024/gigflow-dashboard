77bf87c10c84b0aafcc3f20c1301716c92348030
Gargi Srivastava <gargi.srivastava2024@nst.rishihood.edu.in>
2026-05-16 16:25:52 +0530

    Prepare backend for deployment

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,7 +7,11 @@ import leadRoutes from "./routes/leadRoutes";
 
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+      origin: "*",
+    })
+  );
 app.use(express.json());
 
 app.get("/", (req, res) => {
