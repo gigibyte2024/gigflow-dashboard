cc3453b537316b1aab1af62f6948915ec915ec0c
Gargi Srivastava <gargi.srivastava2024@nst.rishihood.edu.in>
2026-05-16 11:40:35 +0530

    Setup MongoDB connection

---
+++ server/src/config/db.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      process.env.MONGO_URI as string
+    );
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default connectDB;
