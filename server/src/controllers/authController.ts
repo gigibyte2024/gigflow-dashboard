23fab35e138d21c83076b91322d4e60034af8769
Gargi Srivastava <gargi.srivastava2024@nst.rishihood.edu.in>
2026-05-16 12:05:16 +0530

    Add user registration logic

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,12 +1,41 @@
 import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+
+import User from "../models/User";
 
 export const registerUser = async (
   req: Request,
   res: Response
 ) => {
-  res.json({
-    message: "Register Route",
-  });
+  try {
+    const { name, email, password, role } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
 };
 
 export const loginUser = async (
